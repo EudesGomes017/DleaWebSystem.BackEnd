4988c0077df56735959dfa04c5dc4b2a1f938b7f
EudesGomes017 <eudesgomes017@gmail.com>
2024-03-26 14:29:08 -0300

    Create method findById

--- src/main/java/com/dleawebsystem/dslist/controllers/GameController.java
+++ src/main/java/com/dleawebsystem/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dleawebsystem.dslist.dto.GameDTO;
 import com.dleawebsystem.dslist.dto.GameMinDTO;
 import com.dleawebsystem.dslist.services.GameService;
 
@@ -15,11 +17,19 @@ import com.dleawebsystem.dslist.services.GameService;
 public class GameController {
 
 	@Autowired
-	private GameService gameService; 
+	private GameService gameService;
+
 	@GetMapping
 	public List<GameMinDTO> findAll() {
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
+	}
+
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+
 	}
 
 }
---
+++ src/main/java/com/dleawebsystem/dslist/dto/GameDTO.java
@@ -0,0 +1,103 @@
+package com.dleawebsystem.dslist.dto;
+
+import org.springframework.beans.BeanUtils;
+
+import com.dleawebsystem.dslist.entities.Game;
+
+//Utilizando o pdrão dto  deixamos os dtos retornando para nossas requisições, evitamos repetições ciclicas 
+//caso nossa classe game tivesse uma relacionamento por exemplo OneToOne entre as duas entidades.
+public class GameDTO {
+
+	private Long id;
+	private String title;
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+
+	public GameDTO() {
+
+	}
+
+	public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this); // para fazer a transferencia de objetos das entidades com mesmo nomes e quantidades de atributos 
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPlatforms() {
+		return platforms;
+	}
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+	
+	
+
+}
